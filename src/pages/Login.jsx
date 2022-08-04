5ebf0c3dd12ed0169058b1cb60401e5534b0c62e
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-04 16:25:47 -0700

    adds firebase authentication boilerplate to firebase.js and handlechange function to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,35 @@
 import './login.scss';
 import {useState} from 'react';
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase"
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+    
+  }
   return (
     <div className="login">
-      <form>
-    <input type="email" placeholder="email" />
-    <input type="password" placeholder="password" />
+      <form onSubmit={handleLogin}>
+    <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
     <button type="submit">Login</button>
-    <span>Wrong email or password!</span>
+    {error ? <span>Wrong email or password!</span> : false}
       </form>
     </div>
   )
