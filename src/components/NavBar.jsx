ab75f3e0e14047b54af0f857de0f146085db3fe9
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-04 18:17:09 -0700

    adds login link to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      
+      <Link to='/login'>Login</Link>
+    </div>
   )
 }
 
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,19 +1,35 @@
 import { useContext, createContext } from "react";
 import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged } from "firebase/auth";
-import { auth } from '../firebase'
+import { auth } from '../firebase';
 import { useEffect } from "react";
-
+import { useState } from 'react';
 
 const AuthContext = createContext();
+
 export const AuthContextProvider = ({children}) => {
+  const [user, setUser] = useState({})
   const googleLogin = () => {
     const provider = new GoogleAuthProvider()
     signInWithPopup(auth, provider)
   };
 
+ const logOut = () => {
+  signOut(auth)
+ }
+
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+      console.log('User', currentUser)
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{googleLogin}}>
+    <AuthContext.Provider value={{googleLogin, logOut, user}}>
       {children}
     </AuthContext.Provider>
   )
